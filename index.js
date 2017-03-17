49f8958ed0feaf3a67dddedba0fd46e0bd2ab124
Christian Angermann <christian.angermann@wlw.de>
2017-03-17 01:13:40 +0100

    Render palette in larger resolution

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 // import Generator from './lib/js/generator.js'
+const path = require('path')
+const chalk = require('chalk')
+const svg2png = require('svg2png')
+const fs = require('fs')
+
 const VimColorGenerator = require('./lib/js/vim_color_generator.js')
 const SVGGenerator = require('./lib/js/svg_generator.js')
 
-const path = require('path')
-const svg2png = require('svg2png')
-const fs = require('fs')
 const vimColorGenerator = new VimColorGenerator()
 const svgPrinter = new SVGGenerator()
 const themePath = './colors/bronkow/material'
 
 fs.writeFile(path.join(themePath, 'colors.vim'), vimColorGenerator.run(), (err) => {
   if (err) throw err
-  console.log('Creates vim colors file!')
+  console.log(chalk.grey('[1/3]'), 'Creating vim colors file...')
 })
-
 
 fs.writeFile(path.join(themePath, 'palette.svg'), svgPrinter.run(), (err) => {
   if (err) throw err
-  console.log('Creates SVG Palette!')
+  console.log(chalk.grey('[2/3]'), 'Creating SVG palette...')
 
   fs.readFile(path.join(themePath, 'palette.svg'), (err, data) => {
     if (err) throw err
 
     svg2png(data).then(buffer => fs.writeFile(path.join(themePath, 'palette.png'), buffer, (err) => {
       if (err) throw err
-      console.log('Converts SVG Palette to PNG!')
+      console.log(chalk.grey('[3/3]'), 'Converting SVG palette to PNG...')
     }))
   })
 })
